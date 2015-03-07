f446e62232a98f138a98adff9a9c3ccf99c52058
zabaglione <z.zabaglione@gmail.com>
2015-03-08 00:18:36 +0900

    初登録

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # TextureScale
+
+概要
+
+Oculus SDK 0.4.4において外部からTextuteScaleを変更できる機能を追加する。
+
+使い方
+
+* OculusUnityIntegration.unitypackageを取り込む
+* TextureScale.csを追加する
+* dataフォルダをAssetsのトップにコピーする
+* data/ConfigTextureScale.txtを適当に編集する
+*OVRManager.csの325行目に以下の行を加える
+
+'
+        TextureScale.InitTextureScale();
+'
+
+'
+==== OVRManager.cs:313～327行目のイメージ ===
+
+	private void Awake()
+	{
+		// Only allow one instance at runtime.
+		if (instance != null)
+		{
+			enabled = false;
+			DestroyImmediate(this);
+			return;
+		}
+
+		instance = this;
+
+        TextureScale.InitTextureScale();
+
+#if !UNITY_ANDROID || UNITY_EDITOR
+		if (!ovrIsInitialized)
+
+===============================
+'
+
+* UnityEditor上で実行し動作確認をする
+
+* アプリをビルドする
+* dataフォルダをアプリと同じところにコピーする(ファイルではなく、フォルダごとコピー)
+
+'
+====設定後のイメージ(Windows)====
+    OculusApp.exe
+    OculusApp_DirectToRift.exe
+    OculusApp_Data\
+    data\
+
+====設定後のイメージ(Mac OS X)====
+    OculusApp.app
+    data/
+'
+
+* アプリ実行前にdata/ConfigTextureScale.txtファイルを確認
+* アプリを実行
---
+++ TextureScale.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO; //System.IO.FileInfo, System.IO.StreamReader, System.IO.StreamWriter
+using System; //Exception
+using System.Text; //Encoding
+using Ovr;
+
+public class TextureScale  {
+
+	public static string configTextureScale = "ConfigTextureScale.txt";
+
+	public static void InitTextureScale(){
+		String url;
+		if (Application.platform == RuntimePlatform.OSXEditor ||
+		    Application.platform == RuntimePlatform.WindowsEditor){
+			url = Application.dataPath + "/data/" + configTextureScale;
+		}else if (Application.platform == RuntimePlatform.OSXPlayer){
+			url = Application.dataPath + "/../../data/" + configTextureScale;
+		}else if (Application.platform == RuntimePlatform.WindowsPlayer){
+			url = Application.dataPath + "/../data/" + configTextureScale;
+		}else{
+			url = "";
+		}
+
+		FileInfo fi = new FileInfo(url);
+		try {
+			using (TextReader reader = new StreamReader(fi.FullName, Encoding.UTF8))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					string[] data = line.Split(',');
+					Debug.Log (data[0] + " " + data[1]);
+					switch(data[0]) {
+					case "nativeTextureScale":
+						OVRManager.instance.nativeTextureScale = float.Parse(data[1]);
+						break;
+					case "virtualTextureScale":
+						OVRManager.instance.virtualTextureScale = float.Parse(data[1]);
+						break;
+					}
+				}
+			}
+		} catch(Exception e) {
+			//Debug.Log (e.StackTrace);
+		}
+	}
+}
